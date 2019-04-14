ef347c04a93dd8eba85c886fff7d9213c07f1e14
RRpaids <tengfeiqaq@gmail.com>
2019-04-15 07:24:08 +0800

    红绿灯

---
+++ Multithreading/src/main/MR/TurnColor.java
@@ -0,0 +1,40 @@
+package main.MR;
+
+import javax.swing.*;
+
+/**
+ * 交通红绿灯
+ * 2019/4/15
+ * @author 腾飞
+ */
+public class TurnColor extends Thread {
+    private JLabel bgLabel;
+
+    public void setBgLabel(JLabel bgLabel) {
+        this.bgLabel = bgLabel;
+    }
+
+    @Override
+    public void run() {
+        while (true) { // 线程始终处于被启用状态
+            try {
+                Thread.sleep(2000); // 线程休眠5秒
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            bgLabel.setIcon(new ImageIcon(TraficLights.class.getResource("/resources/Yellow.png"))); // 黄灯
+            try {
+                Thread.sleep(2000); // 线程休眠2秒
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            bgLabel.setIcon(new ImageIcon(TraficLights.class.getResource("/resources/Red.png"))); // 红灯
+            try {
+                Thread.sleep(2000); // 线程休眠8秒
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            bgLabel.setIcon(new ImageIcon(TraficLights.class.getResource("/resources/Green.png"))); // 绿灯
+        }
+    }
+}
