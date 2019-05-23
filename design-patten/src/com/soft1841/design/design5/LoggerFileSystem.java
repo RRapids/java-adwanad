9414c86c10284ecdee2c567fc2f0123870c84231
RRpaids <tengfeiqaq@gmail.com>
2019-05-24 00:26:28 +0800

    装饰器模式

---
+++ design-patten/src/com/soft1841/design/design5/LoggerFileSystem.java
@@ -0,0 +1,9 @@
+package com.soft1841.design.design5;
+
+public class LoggerFileSystem implements Logger {
+
+    @Override
+    public void log(String message) {
+        System.out.println("日志输出到本地文件系统："+message);
+    }
+}
